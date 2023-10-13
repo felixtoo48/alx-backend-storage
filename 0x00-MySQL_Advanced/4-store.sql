7a7605e8b8c70168587f04d2ce3ac0d638909930
felixtoo48 <felixtoo48@gmail.com>
2023-10-13 17:18:15 +0000

    initializing order and item tables

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+--  creating a trigge that decreases an item quantity
+-- after an order of the item is made
+
+CREATE TRIGGER decrease_item 
+AFTER INSERT ON orders FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
