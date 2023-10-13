f9389df7a1393c941919b190e2481bdbee2d0527
felixtoo48 <felixtoo48@gmail.com>
2023-10-13 16:10:44 +0000

    upating date

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- as their main style, ranked by their longevity
 
 SELECT band_name,
-	COALESCE(split, 2020) - formed AS lifespan
+	COALESCE(split, 2022) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
