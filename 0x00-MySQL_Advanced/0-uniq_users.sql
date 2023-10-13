34195f8e52d06011eb6067183b584e5b21f849ff
felixtoo48 <felixtoo48@gmail.com>
2023-10-13 12:56:49 +0000

    creating table users with attributes

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- create table users
+-- attributes id email and name
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id)
+);
