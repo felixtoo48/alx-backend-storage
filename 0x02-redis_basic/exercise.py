b72f17017082a0cfbe6fdaa3cbbcfae7f5526651
felixtoo48 <felixtoo48@gmail.com>
2023-10-18 21:56:29 +0000

    exercise

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,52 @@
 #!/usr/bin/env python3
 """ redis-py exercise """
 import redis
+import sys
 import uuid
-from typing import Union, Callable
+from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """count number of calls made to a method"""
+    key = method.__qualname__
+
+    @wraps(method)
+    def counter(self, *args, **kwargs):
+        """decorator method"""
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return counter
+
+
+def call_history(method: Callable) -> Callable:
+    """store the history of inputs and outputs for a particular function"""
+    @wraps(method)
+    def history_wrapper(self, *args, **kwargs):
+        """set list keys to a wrapped function"""
+        in_list_key = method.__qualname__ + ":inputs"
+        out_list_key = method.__qualname__ + ":outputs"
+        self._redis.rpush(in_list_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(out_list_key, str(output))
+        return output
+    return history_wrapper
+
+
+def replay(method: Callable) -> None:
+    """function displays the history of calls of a particular function"""
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+    server = method.__self__._redis
+    count = server.get(key).decode("utf-8")
+    print(f"{key} was called {count} times:")
+    input_list = server.lrange(inputs, 0, -1)
+    output_list = server.lrange(outputs, 0, -1)
+    zipped = list(zip(input_list, output_list))
+    for k, v in zipped:
+        attr, result = k.decode("utf-8"), k.decode("utf-8")
+        print(f"{key}(*{attr}) -> {result}")
 
 
 class Cache():
