b766ab05785d7d64cb349950d57007292c8251ac
felixtoo48 <felixtoo48@gmail.com>
2023-10-18 20:52:42 +0000

    adding get method returning data in required format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -22,3 +22,25 @@ class Cache():
         self._redis.set(random_key, data)
         # Return the random key as a string
         return random_key
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
+        """ method that takes key argument and an optinal callable argument fn 
+        and return converted data back to desired format"""
+        # Get the value from Redis
+        value = self._redis.get(key)
+
+        if value is not None:
+            # If a conversion function is provided, apply it
+            if fn is not None:
+                return fn(value)
+
+            # If no conversion function is provided, return the value as is
+            return value
+
+        return None
+    
+    def get_str(self, key: str) -> Union[str, bytes, None]:
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        return self.get(key, fn=int)
